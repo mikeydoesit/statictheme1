93d38cf7ba2ca98e8a1ed837b5612eae896aa3e8
Michael Squire Obeng <michaelsquireobeng@gmail.com>
2021-11-14 23:53:04 +0000

    hover effect on collection 1

--- components/Collections/Collection1/Collection1.js
+++ components/Collections/Collection1/Collection1.js
@@ -5,35 +5,35 @@ export default function Collection1() {
                 <div className="row">
                     <div className="col-md-4">
                         <div className="collection1__product-image">
-                            <img src="https://www.accraecommerce.com/htmlemailimages/candle2.png" alt="" />
-                            <img src="https://www.accraecommerce.com/htmlemailimages/candle1.png" alt="" />
+                            <span className="collection1__heart"><i className="far fa-heart fa-lg"></i></span>
+                            <img src="https://www.accraecommerce.com/htmlemailimages/candle1.png" alt="" className="collection1__back-img"/>
+                            <img src="https://www.accraecommerce.com/htmlemailimages/candle2.png" alt="" className="collection1__front-img"/>
                         </div>
                         <div className="collection1__product-details">
                             <span className="collection1__product-name">Deep Curl Brazilian Hair</span>
                             <span className="collection1__product-price">39.99</span>
-                            <span className="collection1__heart"><i className="far fa-heart"></i></span>
                         </div>
                     </div>
                     <div className="col-md-4">
                         <div className="collection1__product-image">
-                            <img src="https://www.accraecommerce.com/htmlemailimages/candle4.png" alt="" />
-                            <img src="https://www.accraecommerce.com/htmlemailimages/candle3.png" alt="" />
+                            <span className="collection1__heart"><i className="far fa-heart fa-lg"></i></span>
+                            <img src="https://www.accraecommerce.com/htmlemailimages/candle3.png" alt="" className="collection1__back-img"/>
+                            <img src="https://www.accraecommerce.com/htmlemailimages/candle4.png" alt="" className="collection1__front-img"/>
                         </div>
                         <div className="collection1__product-details">
                         <span className="collection1__product-name">Deep Curl Brazilian Hair</span>
                         <span className="collection1__product-price">39.99</span>
-                        <span className="collection1__heart"><i className="far fa-heart"></i></span>
                         </div>
                     </div>
                     <div className="col-md-4">
                         <div className="collection1__product-image">
-                            <img src="https://www.accraecommerce.com/htmlemailimages/candle6.png" alt="" />
-                            <img src="https://www.accraecommerce.com/htmlemailimages/candle5.png" alt="" />
+                            <span className="collection1__heart"><i className="far fa-heart fa-lg"></i></span>
+                            <img src="https://www.accraecommerce.com/htmlemailimages/candle5.png" alt="" className="collection1__back-img"/>
+                            <img src="https://www.accraecommerce.com/htmlemailimages/candle6.png" alt="" className="collection1__front-img"/>
                         </div>
                         <div className="collection1__product-details">
                         <span className="collection1__product-name">Premium Blend Indian</span>
                         <span className="collection1__product-price">49.99</span>
-                        <span className="collection1__heart"><i className="far fa-heart"></i></span>
                         </div>
                     </div>
                 </div>
--- components/Hero/Hero2/Hero2.js
+++ components/Hero/Hero2/Hero2.js
@@ -2,7 +2,7 @@ export default function Hero2() {
     return (
         <div className="hero hero2">
         <div className="hero2__collection hero2__align-right">
-            <img src="https://www.accraecommerce.com/htmlemailimages/hero1bg.png" alt="Beautiful female model" className="hero2__bg hero2__bg-position-left" />
+            <img src="https://www.accraecommerce.com/htmlemailimages/framesbg.png" alt="Beautiful female model" className="hero2__bg hero2__bg-position-left" />
             <div className="hero2__content-container">
                 <div className="hero2__info">
                     <a className="hero2__button">Frames</a>
@@ -10,7 +10,7 @@ export default function Hero2() {
             </div>
         </div>
         <div className="hero2__collection hero2__align-left">
-        <img src="https://www.accraecommerce.com/htmlemailimages/hero2bg.png" alt="Beautiful female model" className="hero2__bg hero2__bg-position-right" />
+        <img src="https://www.accraecommerce.com/htmlemailimages/candlesbg.png" alt="Beautiful female model" className="hero2__bg hero2__bg-position-right" />
         <div className="hero2__content-container">
             <div className="hero2__info">
                 <a className="hero2__button">Candles</a>
