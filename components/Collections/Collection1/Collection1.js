11d12dde47c10796c61a9927fe4e95adadb18a38
Michael Squire Obeng <michaelsquireobeng@gmail.com>
2021-11-15 11:11:34 +0000

    collection image alignment

--- components/Collections/Collection1/Collection1.js
+++ components/Collections/Collection1/Collection1.js
@@ -4,36 +4,45 @@ export default function Collection1() {
             <div className="container">
                 <div className="row">
                     <div className="col-md-4">
-                        <div className="collection1__product-image">
-                            <span className="collection1__heart"><i className="far fa-heart fa-lg"></i></span>
-                            <img src="https://www.accraecommerce.com/htmlemailimages/candle1.png" alt="" className="collection1__back-img"/>
-                            <img src="https://www.accraecommerce.com/htmlemailimages/candle2.png" alt="" className="collection1__front-img"/>
-                        </div>
-                        <div className="collection1__product-details">
-                            <span className="collection1__product-name">Deep Curl Brazilian Hair</span>
-                            <span className="collection1__product-price">39.99</span>
-                        </div>
-                    </div>
-                    <div className="col-md-4">
-                        <div className="collection1__product-image">
-                            <span className="collection1__heart"><i className="far fa-heart fa-lg"></i></span>
-                            <img src="https://www.accraecommerce.com/htmlemailimages/candle3.png" alt="" className="collection1__back-img"/>
-                            <img src="https://www.accraecommerce.com/htmlemailimages/candle4.png" alt="" className="collection1__front-img"/>
-                        </div>
-                        <div className="collection1__product-details">
-                        <span className="collection1__product-name">Deep Curl Brazilian Hair</span>
-                        <span className="collection1__product-price">39.99</span>
+                        <div className="collection1__collection-item">
+                            <div className="collection1__product-image">
+                                <span className="collection1__heart"><i className="far fa-heart fa-lg"></i></span>
+                                <img src="https://www.accraecommerce.com/htmlemailimages/candle1.png" alt="" className="collection1__back-img"/>
+                                <img src="https://www.accraecommerce.com/htmlemailimages/candle2.png" alt="" className="collection1__front-img"/>
+                            </div>
+                            <div className="collection1__product-details">
+                                <span className="collection1__product-name">Refresh</span>
+                                <span className="collection1__product-cat">Lime, Basil & Mandarin</span>
+                                <span className="collection1__product-price">39.99</span>
+                            </div>
                         </div>
                     </div>
                     <div className="col-md-4">
-                        <div className="collection1__product-image">
-                            <span className="collection1__heart"><i className="far fa-heart fa-lg"></i></span>
-                            <img src="https://www.accraecommerce.com/htmlemailimages/candle5.png" alt="" className="collection1__back-img"/>
-                            <img src="https://www.accraecommerce.com/htmlemailimages/candle6.png" alt="" className="collection1__front-img"/>
+                        <div className="collection1__collection-item">
+                            <div className="collection1__product-image">
+                                <span className="collection1__heart"><i className="far fa-heart fa-lg"></i></span>
+                                <img src="https://www.accraecommerce.com/htmlemailimages/candle3.png" alt="" className="collection1__back-img"/>
+                                <img src="https://www.accraecommerce.com/htmlemailimages/candle4.png" alt="" className="collection1__front-img"/>
+                            </div>
+                            <div className="collection1__product-details">
+                                <span className="collection1__product-name">Bora Bora</span>
+                                <span className="collection1__product-cat">Cranberry & Pomegranate</span>
+                                <span className="collection1__product-price">39.99</span>
+                            </div>
                         </div>
-                        <div className="collection1__product-details">
-                        <span className="collection1__product-name">Premium Blend Indian</span>
-                        <span className="collection1__product-price">49.99</span>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="collection1__collection-item">
+                            <div className="collection1__product-image">
+                                <span className="collection1__heart"><i className="far fa-heart fa-lg"></i></span>
+                                <img src="https://www.accraecommerce.com/htmlemailimages/candle5.png" alt="" className="collection1__back-img"/>
+                                <img src="https://www.accraecommerce.com/htmlemailimages/candle6.png" alt="" className="collection1__front-img"/>
+                            </div>
+                            <div className="collection1__product-details">
+                                <span className="collection1__product-name">Sweet Nectar</span>
+                                <span className="collection1__product-cat">Hibiscus & Peach</span>
+                                <span className="collection1__product-price">49.99</span>
+                            </div>
                         </div>
                     </div>
                 </div>
--- components/Collections/Collection2/Collection2.js
+++ components/Collections/Collection2/Collection2.js
@@ -4,43 +4,59 @@ export default function Collection2() {
             <div className="container">
                 <div className="row">
                     <div className="col-md-3">
-                        <div className="collection2__product-image">
-                            <img src="https://www.accraecommerce.com/htmlemailimages/decor2.png" alt="" />
-                        </div>
-                        <div className="collection2__product-details">
-                        <span className="collection2__product-name">Deep Curl Brazilian Hair</span>
-                        <span className="collection2__product-price">39.99</span>
-                        <span className="collection2__heart"><i className="far fa-heart"></i></span>
-                        </div>
-                    </div>
-                    <div className="col-md-3">
-                        <div className="collection2__product-image">
-                            <img src="https://www.accraecommerce.com/htmlemailimages/decor4.png" alt="" />
-                        </div>
-                        <div className="collection2__product-details">
-                        <span className="collection2__product-name">Deep Curl Brazilian Hair</span>
-                        <span className="collection2__product-price">39.99</span>
-                        <span className="collection2__heart"><i className="far fa-heart"></i></span>
+                        <div className="collection2__collection-item">
+                            <div className="collection2__product-image">
+                                <span className="collection2__heart"><i className="far fa-heart fa-lg"></i></span>
+                                <img src="https://www.accraecommerce.com/htmlemailimages/decor1.png" alt="" className="collection2__back-img"/>
+                                <img src="https://www.accraecommerce.com/htmlemailimages/decor2.png" alt="" className="collection2__front-img"/>
+                            </div>
+                            <div className="collection2__product-details">
+                                <span className="collection2__product-name">Clepsydris</span>
+                                <span className="collection2__product-cat">Decorative Hourglass</span>
+                                <span className="collection2__product-price">39.99</span>
+                            </div>
                         </div>
                     </div>
                     <div className="col-md-3">
-                        <div className="collection2__product-image">
-                            <img src="https://www.accraecommerce.com/htmlemailimages/frame2.png" alt="" />
-                        </div>
-                        <div className="collection2__product-details">
-                        <span className="collection2__product-name">Premium Blend Indian</span>
-                        <span className="collection2__product-price">49.99</span>
-                        <span className="collection2__heart"><i className="far fa-heart"></i></span>
+                        <div className="collection2__collection-item">
+                            <div className="collection2__product-image">
+                                <span className="collection2__heart"><i className="far fa-heart fa-lg"></i></span>
+                                <img src="https://www.accraecommerce.com/htmlemailimages/decor3.png" alt="" className="collection2__back-img"/>
+                                <img src="https://www.accraecommerce.com/htmlemailimages/decor4.png" alt="" className="collection2__front-img"/>
+                            </div>
+                            <div className="collection2__product-details">
+                                <span className="collection2__product-name">Noctua</span>
+                                <span className="collection2__product-cat">Set of 3 owl totems</span>
+                                <span className="collection2__product-price">39.99</span>
+                            </div>
                         </div>
                     </div>
                     <div className="col-md-3">
-                        <div className="collection2__product-image">
-                            <img src="https://www.accraecommerce.com/htmlemailimages/frame4.png" alt="" />
+                        <div className="collection2__collection-item">
+                            <div className="collection2__product-image">
+                                <span className="collection2__heart"><i className="far fa-heart fa-lg"></i></span>
+                                <img src="https://www.accraecommerce.com/htmlemailimages/frame3.png" alt="" className="collection2__back-img"/>
+                                <img src="https://www.accraecommerce.com/htmlemailimages/frame4.png" alt="" className="collection2__front-img"/>
+                            </div>
+                            <div className="collection2__product-details">
+                                <span className="collection2__product-name">Tarandro</span>
+                                <span className="collection2__product-cat">Reindeer Canvas Print</span>
+                                <span className="collection2__product-price">39.99</span>
+                            </div>
                         </div>
-                        <div className="collection2__product-details">
-                        <span className="collection2__product-name">New Soft Swiss Lace Front</span>
-                        <span className="collection2__product-price">64.99</span>
-                        <span className="collection2__heart"><i className="far fa-heart"></i></span>
+                    </div>
+                    <div className="col-md-3">
+                        <div className="collection2__collection-item">
+                            <div className="collection2__product-image">
+                                <span className="collection2__heart"><i className="far fa-heart fa-lg"></i></span>
+                                <img src="https://www.accraecommerce.com/htmlemailimages/frame1.png" alt="" className="collection2__back-img"/>
+                                <img src="https://www.accraecommerce.com/htmlemailimages/frame2.png" alt="" className="collection2__front-img"/>
+                            </div>
+                            <div className="collection2__product-details">
+                                <span className="collection2__product-name">Sidereum</span>
+                                <span className="collection2__product-cat">Starry Night Canvas Print</span>
+                                <span className="collection2__product-price">49.99</span>
+                            </div>
                         </div>
                     </div>
                 </div>
