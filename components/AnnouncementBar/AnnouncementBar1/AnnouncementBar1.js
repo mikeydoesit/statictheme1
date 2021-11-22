07ce8f4256898740c676b24fa5c06bafa5bdb3ae
Michael Squire Obeng <msquireobeng@gmail.com>
2021-11-22 18:17:58 +0000

    got my stuff back after being burgled

--- components/AnnouncementBar/AnnouncementBar1/AnnouncementBar1.js
+++ components/AnnouncementBar/AnnouncementBar1/AnnouncementBar1.js
@@ -1,7 +1,7 @@
 export default function AnnouncementBar1() {
     return (
         <div className="header__promotion-banner">
-            <span>Free shipping on all orders over $60</span>
+            <span>Free shipping on all orders over $75</span>
         </div>
         )
 }
