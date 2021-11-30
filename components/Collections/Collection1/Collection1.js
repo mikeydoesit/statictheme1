29e1109fd080b9196248498f69726b170b36a65f
Michael Squire Obeng <msquireobeng@gmail.com>
2021-11-30 17:30:49 +0000

    tweaked product card

--- components/Collections/Collection1/Collection1.js
+++ components/Collections/Collection1/Collection1.js
@@ -3,12 +3,14 @@ export default function Collection1() {
         <div className="collection1">
             <div className="container">
                 <div className="row">
-                    <div className="col-md-4">
+                    <div className="col-md-2">
                         <div className="collection1__collection-item">
                             <div className="collection1__product-image">
                                 <span className="collection1__heart"><i className="far fa-heart fa-lg"></i></span>
-                                <img src="https://www.accraecommerce.com/htmlemailimages/candle1.png" alt="" className="collection1__back-img"/>
-                                <img src="https://www.accraecommerce.com/htmlemailimages/candle2.png" alt="" className="collection1__front-img"/>
+                                <a className="collection1__product-link" href="/">
+                                    <img src="https://www.accraecommerce.com/htmlemailimages/candle1.png" alt="" className="collection1__back-img"/>
+                                    <img src="https://www.accraecommerce.com/htmlemailimages/candle2.png" alt="" className="collection1__front-img"/>
+                                </a>
                             </div>
                             <div className="collection1__product-details">
                                 <span className="collection1__product-name">Refresh</span>
@@ -17,12 +19,14 @@ export default function Collection1() {
                             </div>
                         </div>
                     </div>
-                    <div className="col-md-4">
+                    <div className="col-md-2">
                         <div className="collection1__collection-item">
                             <div className="collection1__product-image">
                                 <span className="collection1__heart"><i className="far fa-heart fa-lg"></i></span>
+                                <a className="collection1__product-link" href="/">
                                 <img src="https://www.accraecommerce.com/htmlemailimages/candle3.png" alt="" className="collection1__back-img"/>
                                 <img src="https://www.accraecommerce.com/htmlemailimages/candle4.png" alt="" className="collection1__front-img"/>
+                                </a>
                             </div>
                             <div className="collection1__product-details">
                                 <span className="collection1__product-name">Bora Bora</span>
@@ -31,12 +35,14 @@ export default function Collection1() {
                             </div>
                         </div>
                     </div>
-                    <div className="col-md-4">
+                    <div className="col-md-2">
                         <div className="collection1__collection-item">
                             <div className="collection1__product-image">
                                 <span className="collection1__heart"><i className="far fa-heart fa-lg"></i></span>
+                                <a className="collection1__product-link" href="/">
                                 <img src="https://www.accraecommerce.com/htmlemailimages/candle5.png" alt="" className="collection1__back-img"/>
                                 <img src="https://www.accraecommerce.com/htmlemailimages/candle6.png" alt="" className="collection1__front-img"/>
+                                </a>
                             </div>
                             <div className="collection1__product-details">
                                 <span className="collection1__product-name">Sweet Nectar</span>
