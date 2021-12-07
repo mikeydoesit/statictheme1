42a6ca9323d63f64672e6b9f7fcd67f4fda3ccf3
Michael Squire Obeng <msquireobeng@gmail.com>
2021-12-07 20:58:04 +0000

    added classes for transparent and rixed nav

--- components/layouts/MainLayout.js
+++ components/layouts/MainLayout.js
@@ -1,18 +1,16 @@
 import Header1 from "../Header/Header1/Header1";
 import Header2 from "../Header/Header2/Header2";
 import Header3 from "../Header/Header3/Header3";
-import Footer from "../Footer/Footer"
-import AnnouncementBar1 from "../AnnouncementBar/AnnouncementBar1/AnnouncementBar1";
+import Footer from "../Footer/Footer";
 
 export default function MainLayout(props) {
     return (
-        <>  
-            <AnnouncementBar1 />
+        <div className="fixed-nav transparent-nav">
             <Header1 />
             {/* <Header2 />
             <Header3 /> */}
                 <main>{props.children}</main>
             <Footer />
-        </>
+        </div>
     )
 }
