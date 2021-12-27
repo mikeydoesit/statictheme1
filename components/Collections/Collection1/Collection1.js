6919726c34cc47497321255ca5b5c391620b0b29
Michael Squire Obeng <msquireobeng@gmail.com>
2021-12-27 00:23:49 +0000

    extra components 0%

--- components/Collections/Collection1/Collection1.js
+++ components/Collections/Collection1/Collection1.js
@@ -51,6 +51,54 @@ export default function Collection1() {
                             </div>
                         </div>
                     </div>
+                    <div className="col-md-2">
+                        <div className="collection1__collection-item">
+                            <div className="collection1__product-image">
+                                <span className="collection1__heart"><i className="far fa-heart fa-lg"></i></span>
+                                <a className="collection1__product-link" href="/">
+                                <img src="https://www.accraecommerce.com/htmlemailimages/candle8.png" alt="" className="collection1__back-img"/>
+                                <img src="https://www.accraecommerce.com/htmlemailimages/candle7.png" alt="" className="collection1__front-img"/>
+                                </a>
+                            </div>
+                            <div className="collection1__product-details">
+                                <span className="collection1__product-name">Sweet Nectar</span>
+                                <span className="collection1__product-cat">Hibiscus & Peach</span>
+                                <span className="collection1__product-price">49.99</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-2">
+                        <div className="collection1__collection-item">
+                            <div className="collection1__product-image">
+                                <span className="collection1__heart"><i className="far fa-heart fa-lg"></i></span>
+                                <a className="collection1__product-link" href="/">
+                                <img src="https://www.accraecommerce.com/htmlemailimages/candle10.png" alt="" className="collection1__back-img"/>
+                                <img src="https://www.accraecommerce.com/htmlemailimages/candle9.png" alt="" className="collection1__front-img"/>
+                                </a>
+                            </div>
+                            <div className="collection1__product-details">
+                                <span className="collection1__product-name">Sweet Nectar</span>
+                                <span className="collection1__product-cat">Hibiscus & Peach</span>
+                                <span className="collection1__product-price">49.99</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-2">
+                        <div className="collection1__collection-item">
+                            <div className="collection1__product-image">
+                                <span className="collection1__heart"><i className="far fa-heart fa-lg"></i></span>
+                                <a className="collection1__product-link" href="/">
+                                <img src="https://www.accraecommerce.com/htmlemailimages/candle12.png" alt="" className="collection1__back-img"/>
+                                <img src="https://www.accraecommerce.com/htmlemailimages/candle11.png" alt="" className="collection1__front-img"/>
+                                </a>
+                            </div>
+                            <div className="collection1__product-details">
+                                <span className="collection1__product-name">Sweet Nectar</span>
+                                <span className="collection1__product-cat">Hibiscus & Peach</span>
+                                <span className="collection1__product-price">49.99</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
